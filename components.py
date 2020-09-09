1be0d68603a76d4661d93b8d31a346b19445699f
Gonzalo Ribera <gribera@campus.ungs.edu.ar>
2020-09-09 10:49:14 -0300

    Se agregan nuevos comandos al diccionario: tensión, corriente, potencia (activa, reactiva, aparente), factor de potencia, frecuencia y temperatura

--- components.py
+++ components.py
@@ -1,5 +1,5 @@
-import tkinter as tk 
-from tkinter import * 
+import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 from dialogs import change_address
 from dialogs import connect
@@ -7,26 +7,35 @@ import comm as serial
 # import commands as commands
 
 class Componentes:
-    dicFunctions = {'ERR': '0x01', 'DO': '0x04', 'SET': '0x08', 
-                    'QRY': '0x0c', 'ACK': '0x10', 'POST': '0x14', 
+    dicFunctions = {'ERR': '0x01', 'DO': '0x04', 'SET': '0x08',
+                    'QRY': '0x0c', 'ACK': '0x10', 'POST': '0x14',
                     'HB': '0x18'}
 
     dicDispOrigen = {'Broadcast': '0x00', 'Master': '0x01'}
 
     dicDispDestino = {'Equipo 2': '0x02', 'Equipo 3': '0x03', 'Equipo 4': '0x04'}
 
-    dicVariable = {'DIGITAL_OUT': '0x00', 'DIGITAL_IN': '0x01', 
-                    'ANALOG_OUT': '0x02', 'ANALOG_IN': '0x03', 
+    dicVariable = {'DIGITAL_OUT': '0x00', 'DIGITAL_IN': '0x01',
+                    'ANALOG_OUT': '0x02', 'ANALOG_IN': '0x03',
                     'MODO_FUNC': '0x04', 'ANALOG': '0x05', 'IN-AMP': '0x06',
                     'AMP-INAMP': '0x07','PWM': '0x08', 'ECHO': '0x09',
-                    'RTC1': '0x0A', 'PARADA': '0x0B', 'SOFT_RESET': '0x0C',}
+                    'RTC1': '0x0A', 'PARADA': '0x0B', 'SOFT_RESET': '0x0C',
+                    'U_A': '0x10', 'U_B': '0x11', 'U_C': '0x12',
+                    'I_A': '0x13', 'I_B': '0x14', 'I_C': '0x15', 'I_N1': '0x16',
+                    'PA_A': '0x17', 'PA_B': '0x18', 'PA_C': '0x19', 'PA_TOT': '0x1A',
+                    'PR_A': '0x1B', 'PR_B': '0x1C', 'PR_C': '0x1D', 'PR_TOT': '0x1E',
+                    'PS_A': '0x1F', 'PS_B': '0x20', 'PS_C': '0x21', 'PS_TOT': '0x22',
+                    'FP_A': '0x23', 'FP_B': '0x24', 'FP_C': '0x25', 'FP_TOT': '0x26',
+                    'THDU_A': '0x27', 'THDU_B': '0x28', 'THDU_C': '0x29',
+                    'THDI_A': '0x2A', 'THDI_B': '0x2B', 'THDI_C': '0x2C',
+                    'FREC': '0x2D', 'TEMP': '0x2E'}
 
-    combos = [dicFunctions, dicDispOrigen, dicDispDestino, dicVariable, 
-                list(range(0,255)), list(range(0,255)), list(range(0,255)), 
+    combos = [dicFunctions, dicDispOrigen, dicDispDestino, dicVariable,
+                list(range(0,255)), list(range(0,255)), list(range(0,255)),
                 list(range(0,255)), list(range(0,255)), list(range(0,255))]
 
-    comboLabel = ['Función', 'Origen', 'Destino', 'Variable', 
-                  'Byte 1', 'Byte 2', 'Byte 3', 'Byte 4', 'Byte 5', 
+    comboLabel = ['Función', 'Origen', 'Destino', 'Variable',
+                  'Byte 1', 'Byte 2', 'Byte 3', 'Byte 4', 'Byte 5',
                   'Byte 6', 'Byte 7']
 
     selectedOption = {}
@@ -44,9 +53,9 @@ class Componentes:
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
 
-        status = Label(self.master, 
-                       text="StatusbaR", 
-                       bd=1, 
+        status = Label(self.master,
+                       text="StatusbaR",
+                       bd=1,
                        relief=tk.SUNKEN).grid(row=10, column=0, columnspan=10, sticky='WE')
 
         menuFunciones = Menu(menubar, tearoff=0)
@@ -116,10 +125,10 @@ class Componentes:
             lb = ttk.Label(self.master, text=self.comboLabel[idx])
             lb.grid(row=1, column=idx, padx=10, pady=10)
             # Agrega combo
-            cmbFnc = ttk.Combobox(self.master, 
-                                  values=valores, 
-                                  justify="center", 
-                                  textvariable=self.selectedOption[idx], 
+            cmbFnc = ttk.Combobox(self.master,
+                                  values=valores,
+                                  justify="center",
+                                  textvariable=self.selectedOption[idx],
                                   state="readonly", width=12)
             cmbFnc.bind("<<ComboboxSelected>>", self.drawValues)
             cmbFnc.grid(row=2, column=idx, padx=5, pady=10)
@@ -153,8 +162,8 @@ class Componentes:
 
         # self.scrollbar = tk.Scrollbar(self.frame)
         # self.txtTerminal = tk.Text(self.frame, height=14, width=130)
-        # self.txtTerminal.config(yscrollcommand=self.scrollbar.set, 
-        #                         background="#000000", 
+        # self.txtTerminal.config(yscrollcommand=self.scrollbar.set,
+        #                         background="#000000",
         #                         foreground="#4DFF00",
         #                         font=("Fixedsys", 10),
         #                         padx=3, pady=3)
@@ -190,7 +199,7 @@ class Componentes:
 
     def drawButtons(self):
         self.buttonFrame = Frame(self.master)
-        self.buttonFrame.grid(row=8, column=0, columnspan=10)        
+        self.buttonFrame.grid(row=8, column=0, columnspan=10)
         # self.btnSend = Button(self.buttonFrame, text="Enviar", command=self.canSend)
         # self.btnSend.pack(side=LEFT, padx=30, pady=10)
         # self.btnSend = Button(self.buttonFrame, text="Datos Equipo", command=self.datosEquipo)
