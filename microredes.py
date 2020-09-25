c0a2e116e1a0c9fa73ca88641a18850f9f0224c5
Gonzalo Ribera <gribera@campus.ungs.edu.ar>
2020-09-25 12:43:06 -0300

    Se agrega función para calcular el complemento a 2

--- microredes.py
+++ microredes.py
@@ -20,7 +20,7 @@ class Microredes:
 
         valor = 0
         valueLow = int('0x' + ''.join([format(int(c, 16), '02X') for c in dataLow[2:4]]), 16)
-        valueHigh = int(str(dataHigh[0]), 16)
+        valueHigh = int('0x' + ''.join([format(int(c, 16), '02X') for c in dataHigh[0:1]]), 16)
 
         if variable in calcUrms:
             valor = 0.01*valueLow+valueHigh/256
@@ -32,25 +32,25 @@ class Microredes:
             valor = 0.001*valueLow
             unidad = 'A'
         elif variable in calcPmean:
-            valor = valueLow+valueHigh/256
+            valor = self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256
             unidad = 'W'
         elif variable in calcPmeanT:
-            valor = 4*(valueLow+valueHigh/256)
+            valor = 4*(self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256)
             unidad = 'W'
         elif variable in calcQmean:
-            valor = valueLow+valueHigh/256
+            valor = self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256
             unidad = 'VAr'
         elif variable in calcQmeanT:
-            valor = 4*(valueLow+valueHigh/256)
+            valor = 4*(self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256)
             unidad = 'VAr'
         elif variable in calcSmean:
-            valor = valueLow+valueHigh/256
+            valor = self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256
             unidad = 'VA'
         elif variable in calcSmeanT:
-            valor = 4*(valueLow+valueHigh/256)
+            valor = 4*(self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256)
             unidad = 'VA'
         elif variable in calcPFmean:
-            valor = 0.001*valueLow
+            valor = 0.001*(self.twosComplement(dataLow[2:4] + dataHigh[0:2], 32)/256)
             unidad = 'W'
         elif variable in calcTHDU:
             valor = 0.01*(valueLow)
@@ -63,8 +63,17 @@ class Microredes:
             unidad = 'C'
         else:
             print("La función " + str(variable) + " es incorrecta")
+            return
 
         # Redondea el valor a 3 decimales y lo devuelve en formato string junto con su unidad de medida
         valorFinal = str(round(valor, 3)) + ' ' + unidad
 
         return valorFinal
+
+    def twosComplement(self, value, bits):
+        # Se pasa a hexa el valor recibido
+        val = int('0x' + ''.join([format(int(c, 16), '02X') for c in value]), 16)
+        # Cálculo del complemento a 2
+        if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+            val = val - (1 << bits)        # compute negative value
+        return val                         # return positive value as is
